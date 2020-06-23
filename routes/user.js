316f016b0354c6f937bb52e9e5c3379f5214e078
Naresh Kumar <naresh0101@gmail.com>
2020-06-24 05:21:56 +0530

    login api done

--- routes/user.js
+++ routes/user.js
@@ -10,6 +10,8 @@ let UserAccountController = require("../controllers/useraccount");
 
 router.get("/", (req, res, next) => res.status(200).json({ success: true }));
 
-router.post("/register", UserAccountController.createAccountAndUser);
+router.post("/register", UserAccountController.createUser);
+router.post("/login", UserAccountController.loginAccount);
+
 
 module.exports = router;
